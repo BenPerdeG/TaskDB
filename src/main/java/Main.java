2466e4bc08d6505a83b6cbd0175ca67e96179e5c
DecotDev <decot013@gmail.com>
2025-03-27 19:22:39 +0100

    assdfadqa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.session.IDocumentSession;
+
 public class Main {
     public static void main(String[] args) {
 
+        DocumentStore store = new DocumentStore("http://live-test.ravendb.net", "databaseName");
+        store.initialize();
+
+        try (IDocumentSession session = store.openSession()) {
+            // here goes your code
+        }
     }
 }
