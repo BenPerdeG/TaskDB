3844d9eab6ce152d6f40322f69b82ddc31d132bd
DecotDev <decot013@gmail.com>
2025-04-01 19:25:02 +0200

    DB con working What's new? - DB working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.IDocumentStore;
+import net.ravendb.client.documents.conventions.DocumentConventions;
 import net.ravendb.client.documents.session.IDocumentSession;
-import org.apache.catalina.User;
 
 public class Main {
     public static void main(String[] args) {
 
-        DocumentStore store = new DocumentStore("http://192.168.22.118:8080", "TaskDB");
+        /*DocumentStore store = new DocumentStore("http://192.168.22.118:8080", "TaskDB");
         store.initialize();
 
         try (IDocumentSession session = store.openSession()) {
@@ -15,7 +16,30 @@ public class Main {
             session.saveChanges();
 // data is persisted
 // you can proceed e.g. finish web request
+        }*/
+
+        try (IDocumentStore store = new DocumentStore(new String[]{"http://192.168.22.118:8080"},"TaskDB")) {
+
+            DocumentConventions conventions = store.getConventions();
+
+            store.initialize();
+
+            try (IDocumentSession session = store.openSession()) {
+
+                Task task = new Task();
+                task.setTitle("Task done");
+                task.setDescription("Test of done statues");
+                task.setPriority(1);
+                task.setStatus(Task.Status.DONE);
+
+                session.store(task);
+
+                session.saveChanges();
+            }
+
         }
+
+
 
 
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,51 @@
-@Entity
+//@Entity
 public class Task {
+
+    String title;
+    String description;
+    int priority;
+    public Status status;
+
+    public enum Status {
+        PENDING(0), IN_PROGRESS(1), PAUSED(2), DONE(3);
+        private int value;
+
+        Status(int value) {
+            this.value = value;
+        }
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
