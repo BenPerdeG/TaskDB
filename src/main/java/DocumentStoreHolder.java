2cc6155d7ea79316974aae64a1e2f37d752c6600
dam2a <bperez@elpuig.xeill.net>
2025-04-01 18:58:24 +0200

    Upload. What's new? ajbhfakjdhskjahkjahdaksjdhaksjdhaksdjhaksjdhaksjhdkajshdkajshdkajshdkajshd

--- src/main/java/DocumentStoreHolder.java
+++ src/main/java/DocumentStoreHolder.java
@@ -1,2 +1,16 @@
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.IDocumentStore;
+
 public class DocumentStoreHolder {
+    private static class DocumentStoreContainer {
+        public static final IDocumentStore store =
+                new DocumentStore(new String[]{"http://192.168.22.118:8080"}, "TaskDB");
+        static {
+            store.initialize();
+        };
+        public static IDocumentStore getStore(){
+            return DocumentStoreContainer.store;
+        }
+
+    }
 }
--- src/main/java/RavenDBDocumentStore.java
+++ src/main/java/RavenDBDocumentStore.java
@@ -6,19 +6,14 @@ public final class RavenDBDocumentStore {
     private static IDocumentStore store;
 
     static {
-
-        //Create new Document Store with the url of the RavenDB Server
-        //and with `Hospital` set as the default database
-        store = new DocumentStore(new String[]{"http://192.168.22.118:8080"}, "TaskDB");
-
-        //Edit the conventions to enable optimistic concurrency
+        store =new DocumentStore(new String[]{"http://192.168.22.118:8080"}, "TaskDB");
         DocumentConventions conventions = store.getConventions();
         conventions.setUseOptimisticConcurrency(true);
 
         store.initialize();
     }
 
-    //Return the single Document Store instance
+
     public static IDocumentStore getStore() {
         return store;
     }
