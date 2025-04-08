a919bb563ee5a0b3a97539c4395ed7978f0da62a
dam2a <bperez@elpuig.xeill.net>
2025-04-08 19:58:20 +0200

    Upload. What's new? a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import net.ravendb.client.documents.DocumentStore;
-import net.ravendb.client.documents.IDocumentStore;
-import net.ravendb.client.documents.conventions.DocumentConventions;
-import net.ravendb.client.documents.session.IDocumentSession;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Menu {
     CRUD crud = new CRUD();
-    Scanner sc = new Scanner(System.in);
+
 
     public void startMenu() {
         showWelcome();
@@ -13,20 +13,29 @@ public class Menu {
         System.out.println("\n#####################\n# Welcome to TaskDB #\n#####################\n");
     }
     private int showOptions() {
-        System.out.println("- List of options -");
-        System.out.println(" 1 - List tasks");
-        System.out.println(" 2 - Add a task");
-        System.out.println(" 3 - Modify an existing task");
-        System.out.println(" 4 - Delete a task");
-        System.out.println(" 5 - Exit\n");
-        System.out.print("Your option: ");
-        int option = sc.nextInt();
-        return option;
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("\u001B[0m");
+            System.out.println("- List of options -");
+            System.out.println(" 1 - List tasks");
+            System.out.println(" 2 - Add a task");
+            System.out.println(" 3 - Modify an existing task");
+            System.out.println(" 4 - Delete a task");
+            System.out.println(" 5 - Exit\n");
+            System.out.print("Your option: ");
+            int option = sc.nextInt();
+            return option;
+        }
+        catch(Exception e){
+            System.out.println("\u001B[31m"+"\n\tThere has been an issue with your option select. Please try again with an option from 1-5.");
+            return (showOptions());
+        }
     }
     private void menu(int option) {
         switch (option) {
             case 1:
-                return;
+                listOptions();
+                break;
 
             case 2:
                 return;
@@ -38,11 +47,27 @@ public class Menu {
                 return;
 
             case 5:
-                return;
+                System.out.println("Bye bye ");
+                System.exit(0);
 
             default:
-                System.out.println("Re show menu test");
+                System.out.println("\u001B[31m"+"\n\tThere has been an issue with your option select. Please try again with an option from 1-5.");
                 menu(showOptions());
         }
     }
+
+    private void listOptions() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\u001B[0m");
+        System.out.println("Filter by:\n");
+        System.out.println("\t1-All");
+        System.out.println("\t2-Name");
+        System.out.println("\t3-Priority");
+        System.out.println("\t4-Status");
+        System.out.println("\t5-Go Back");
+
+
+    }
+
+
 }
